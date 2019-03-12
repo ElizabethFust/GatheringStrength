985248dde29ebf737c168cf1ec0b3237f6ac2708
lizfust@iglou.com <lizfust@iglou.com>
2019-03-12 18:25:59 -0400

    updated all files

---
+++ JS/GS.js
@@ -0,0 +1,35 @@
+// Want to create a function that will cause the circle to increase in opacity, as though it is gathering strength.  And do it over and over for a period of time.  Try setInterval() method??
+
+// $('#circle').hide().fadeIn(3000);
+
+// $('#circle').hide().fadeIn()
+//     .animate({height: '150px', opacity: '1'}, "slow")
+//     .animate({width: '150px',
+// opacity: '1'}, "slow");
+
+
+// $('#content').hide().fadeIn(function(){ 
+//     $('#content').animate({
+//         left: "80px"
+//     }, 500); 
+// },1500).css('left' ,'0');
+
+
+
+// $('.crescentSmall').hide()
+//     .animate({opacity: ".8"}, "slow")
+//     .animate({opacity: ".1"}, "slow");
+
+// $('#circle').animate({
+//     opacity: 1,
+// }, 5000,);
+
+
+// $('.crescentSmall').hide().fadeIn(800).delay(1000).fadeOut(500);
+
+// $('.crescentMed').hide().delay(1000).fadeIn(800).delay(1000).fadeOut(500);
+
+// $('.crescentLarge').hide().delay(1500).fadeIn(800).delay(1000).fadeOut(500);
+
+$('#circle').hide().fadeIn(4000).fadeOut(2000).fadeIn(2000).fadeOut(2000).fadeIn(2000);
+
